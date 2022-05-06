fd8982d93fe9a2ce4ab9157df5e2795a75b7d2dd
SAAGAR <38255285+Sagar1555@users.noreply.github.com>
2022-05-06 14:39:08 +0545

    Blog Previews

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,24 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('clicked');
+    }
+    const handleClick2 = () => {
+        console.log('clicked2');
+    }
+
     return ( 
         <div className="home">
             <h2>
                 Homepage
             </h2>
+            <button onClick={handleClick}> Click me </button>
+            <button onClick={(handleClick2)}>Click Me Again </button>
             </div>
         
      );
 }
  
+
 export default Home;
+
